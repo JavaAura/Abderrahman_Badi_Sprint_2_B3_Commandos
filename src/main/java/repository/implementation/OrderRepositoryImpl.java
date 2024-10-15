d330c44b25e0e5eb74a925bf798dfd3616e4d37a
Walid Saifi <saifiwalid21@gmail.com>
2024-10-15 16:00:49 +0100

    ajouter interface repository

--- src/main/java/repository/implementation/OrderRepositoryImpl.java
+++ src/main/java/repository/implementation/OrderRepositoryImpl.java
@@ -1,5 +1,44 @@
 package repository.implementation;
 
-public class OrderRepositoryImpl {
+import model.Order;
+import repository.interfaces.OrderRepository;
 
+import java.util.List;
+
+public class OrderRepositoryImpl implements OrderRepository {
+
+    @Override
+    public List<Order> findOrdersByClient(Long clientId, int page, int size, String searchQuery) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAllOrders(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order updateOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) {
+
+    }
+
+    @Override
+    public Order findOrderById(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public boolean canModifyOrder(Order order) {
+        return false;
+    }
 }
--- src/main/java/repository/interfaces/OrderRepository.java
+++ src/main/java/repository/interfaces/OrderRepository.java
@@ -1,5 +1,22 @@
 package repository.interfaces;
 
+import java.util.List;
+import model.Order;
+import model.enums.Statut;
+
 public interface OrderRepository {
 
+    List<Order> findOrdersByClient(Long clientId, int page, int size, String searchQuery);
+
+    List<Order> findAllOrders(int page, int size);
+
+    Order createOrder(Order order);
+
+    Order updateOrder(Order order) ;
+
+    void deleteOrder(Long orderId);
+
+    Order findOrderById(Long orderId);
+
+    boolean canModifyOrder(Order order);
 }
