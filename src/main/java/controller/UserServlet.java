182391853e1bba722812f1c972f08c29e9dfaac0
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-17 21:15:03 +0100

    CM-42 style: added global bread crumbs

---
+++ src/main/java/controller/UserServlet.java
@@ -0,0 +1,53 @@
+package controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import model.User;
+import model.enums.Role;
+import util.ThymeleafUtil;
+
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		TemplateEngine templateEngine = ThymeleafUtil.getTemplateEngine(request.getServletContext());
+
+		HttpSession session = request.getSession();
+
+		User user = new User();
+		user.setFirstName("admin");
+		user.setEmail("admin@youcode.ma");
+		user.setRole(Role.ADMIN);
+
+		session.setAttribute("user", user);
+
+		User loggedInUser = (User) session.getAttribute("user");
+
+		// Prepare a Thymeleaf context if you need to pass any model data
+		ServletContext servletContext = request.getServletContext();
+		WebContext context = new WebContext(request, response, servletContext, request.getLocale());
+		context.setVariable("user", loggedInUser);
+
+		// Set content type for the response
+		response.setContentType("text/html;charset=UTF-8");
+
+		templateEngine.process("views/dashboard/users", context, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
