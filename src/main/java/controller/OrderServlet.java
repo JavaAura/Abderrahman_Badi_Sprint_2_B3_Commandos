7b036d0c8567c0ba0e5737e275956910619094ae
Walid Saifi <saifiwalid21@gmail.com>
2024-10-17 17:20:50 +0100

    CM-8  add modification de logique de repository V3

--- src/main/java/controller/OrderServlet.java
+++ src/main/java/controller/OrderServlet.java
@@ -1,20 +1,71 @@
 package controller;
 
+import model.Order;
+import model.User;
+import model.enums.Role;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import repository.implementation.OrderRepositoryImpl;
+import repository.interfaces.OrderRepository;
+import service.OrderService;
+import util.ThymeleafUtil;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 public class OrderServlet extends HttpServlet {
+
+	private OrderRepository repository;
+	private OrderService orderService = new OrderService(repository);
+
+
+	@Override
+	public void init() throws ServletException {
+		this.repository = new OrderRepositoryImpl();
+		this.orderService = new OrderService(repository);
+	}
+
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		TemplateEngine templateEngine = ThymeleafUtil.getTemplateEngine(request.getServletContext());
+
+		HttpSession session = request.getSession();
+
+		User user = new User();
+		user.setFirstName("client");
+		user.setEmail("client");
+		user.setRole(Role.CLIENT);
+
+		session.setAttribute("user", user);
+
+		User loggedInUser = (User) session.getAttribute("user");
+
+		ServletContext servletContext = request.getServletContext();
+		WebContext context = new WebContext(request, response, servletContext, request.getLocale());
+		context.setVariable("user", loggedInUser);
+
+		int page = Integer.parseInt(request.getParameter("page") != null ? request.getParameter("page") : "0");
+		int size = 5;
+
+		List<Order> orderList_No_historique = orderService.getAllOrders_No_historique(page, size);
+		context.setVariable("orderList_No_historique", orderList_No_historique);
+
+		response.setContentType("text/html;charset=UTF-8");
+		templateEngine.process("views/dashboard/order", context, response.getWriter());
 	}
+
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 	}
-
 }
--- src/main/java/repository/implementation/OrderRepositoryImpl.java
+++ src/main/java/repository/implementation/OrderRepositoryImpl.java
@@ -1,27 +1,33 @@
 package repository.implementation;
 
 import model.Order;
-import model.Product;
 import repository.interfaces.OrderRepository;
+import util.PersistenceUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class OrderRepositoryImpl implements OrderRepository {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+
 
     @Override
     public List<Order> getByClient(Long clientId, int page, int size, String searchQuery) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+
         String queryStr = "SELECT o FROM Order o WHERE o.client.id = :clientId AND " +
                 "(o.orderStatut LIKE :searchQuery OR o.id LIKE :searchQuery)";
         TypedQuery<Order> query = entityManager.createQuery(queryStr, Order.class);
         query.setParameter("clientId", clientId);
-        query.setParameter("searchQuery", "%" + searchQuery + "%");
 
+
+        if (searchQuery == null) {
+            searchQuery = ""; 
+        }
+
+        query.setParameter("searchQuery", "%" + searchQuery + "%");
         query.setFirstResult(page * size);
         query.setMaxResults(size);
 
@@ -30,23 +36,24 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     @Override
     public List<Order> getAll(int page, int size) {
-        String queryStr = "SELECT o FROM Order o ";
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        String queryStr = "SELECT o FROM Order o";
         TypedQuery<Order> query = entityManager.createQuery(queryStr, Order.class);
         query.setFirstResult(page * size);
         query.setMaxResults(size);
         return query.getResultList();
     }
 
-
-
     @Override
     public Order save(Order order) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
         entityManager.persist(order);
         return order;
     }
 
     @Override
     public Order update(Order order) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
         if (canModify(order)) {
             return entityManager.merge(order);
         } else {
@@ -56,6 +63,7 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     @Override
     public void delete(Long orderId) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
         Order order = get(orderId);
         if (order != null && canModify(order)) {
             entityManager.remove(order);
@@ -66,11 +74,12 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     @Override
     public Order get(Long orderId) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
         return entityManager.find(Order.class, orderId);
     }
 
     @Override
     public boolean canModify(Order order) {
-        return order.getOrderStatut().equals("En attente") || order.getOrderStatut().equals("En traitement");
+        return order.getOrderStatut().equals("WAITING") || order.getOrderStatut().equals("PROCESSING");
     }
 }
