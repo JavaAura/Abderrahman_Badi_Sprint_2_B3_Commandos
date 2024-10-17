718a1caa11bfcc918a20d38bf81a6bce1453ac4b
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-17 09:15:12 +0100

    added Dashboard servlet

--- src/main/java/controller/DashboardServlet.java
+++ src/main/java/controller/DashboardServlet.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class AdminServlet extends HttpServlet {
+public class DashboardServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+				
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
